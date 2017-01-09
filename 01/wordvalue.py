0cb35bda752a08671b88e267ca32a6977e598d98
Johannes Hofmeister <github@account.cessor.de>
2017-01-09 03:27:18 +0100

    Added comments explaining my implementation. I don't like the method naming, but hey, nobody cares.

--- 01/wordvalue.py
+++ 01/wordvalue.py
@@ -13,13 +13,18 @@ def calc_word_value(word):
 def _max_word_value(words=load_words()):
     # Also works, but a tiny slowest
     # Sorting is n log n, but bound to O(n) from input list
-    scores = [(-1 * calc_word_value(word),word) for word in words]
-    scores.sort(key=lambda t: t[0])
+    scores = [(calc_word_value(word),word) for word in words]
+    scores.sort(key=lambda t: t[0], reverse=True)
     return scores[0][1]
 
 def _max_word_value(words=load_words()):
     # Also works, but a tiny bit slower.
     # Sorting is n log n, but bound to O(n) from input list
+    # If already in heap, accessing the largest element is actually O(n)
+    # Note that heapq is a priority queue, that sorts a binary heap
+    # with the smallest element on top. I looked up on stackoverflow how to
+    # make a maxheap: Simply invert the key -_-
+    # http://stackoverflow.com/a/2501527/1203756
     import heapq
     scores = [(-1 * calc_word_value(word),word) for word in words]
     heapq.heapify(scores)
